43c68216e76aac5e0c47ecb3ffaac97f771b5ba1
blerer <b_lerer@hotmail.com>
2014-01-14 16:45:36 +0100

    fix indentation issues

--- model/src/main/java/io/horizondb/model/AbstractRecordBatch.java
+++ model/src/main/java/io/horizondb/model/AbstractRecordBatch.java
@@ -33,145 +33,145 @@ import org.apache.commons.lang.builder.ToStringStyle;
  */
 public abstract class AbstractRecordBatch implements Serializable {
 
-	/**
-	 * The database in which the records must be inserted.
-	 */
-	private final String databaseName;
-	/**
-	 * The time series in which the records must be inserted.
-	 */
-	private final String seriesName;
+    /**
+     * The database in which the records must be inserted.
+     */
+    private final String databaseName;
+    /**
+     * The time series in which the records must be inserted.
+     */
+    private final String seriesName;
 
-	/**
-	 * The partition ID.
-	 */
-	private final long partition;
+    /**
+     * The partition ID.
+     */
+    private final long partition;
 
-	/**
-	 * Creates a new <code>AbstractRecordBatch</code> for the specified database
-	 * and the specified series.
-	 * 
-	 * @param databaseName the database name
-	 * @param seriesName the time series name
-	 * @param partition the partition ID
-	 */
-	public AbstractRecordBatch(String databaseName, String seriesName, long partition) {
+    /**
+     * Creates a new <code>AbstractRecordBatch</code> for the specified database
+     * and the specified series.
+     * 
+     * @param databaseName the database name
+     * @param seriesName the time series name
+     * @param partition the partition ID
+     */
+    public AbstractRecordBatch(String databaseName, String seriesName, long partition) {
 
-		this.databaseName = databaseName;
-		this.seriesName = seriesName;
-		this.partition = partition;
-	}
+        this.databaseName = databaseName;
+        this.seriesName = seriesName;
+        this.partition = partition;
+    }
 
-	/**
-	 * Returns the database name.
-	 * 
-	 * @return the database name.
-	 */
-	public String getDatabaseName() {
-		return this.databaseName;
-	}
+    /**
+     * Returns the database name.
+     * 
+     * @return the database name.
+     */
+    public String getDatabaseName() {
+        return this.databaseName;
+    }
 
-	/**
-	 * Returns the time series name.
-	 * 
-	 * @return the time series name.
-	 */
-	public String getSeriesName() {
-		return this.seriesName;
-	}
+    /**
+     * Returns the time series name.
+     * 
+     * @return the time series name.
+     */
+    public String getSeriesName() {
+        return this.seriesName;
+    }
 
-	/**
-	 * Returns the partition ID.
-	 * 
-	 * @return the partition ID.
-	 */
-	public long getPartition() {
-		return this.partition;
-	}
+    /**
+     * Returns the partition ID.
+     * 
+     * @return the partition ID.
+     */
+    public long getPartition() {
+        return this.partition;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public final int computeSerializedSize() {
-		return VarInts.computeStringSize(this.databaseName) + VarInts.computeStringSize(this.seriesName)
-		        + VarInts.computeUnsignedLongSize(this.partition) + computeRecordSetSerializedSize();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final int computeSerializedSize() {
+        return VarInts.computeStringSize(this.databaseName) + VarInts.computeStringSize(this.seriesName)
+                + VarInts.computeUnsignedLongSize(this.partition) + computeRecordSetSerializedSize();
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public final void writeTo(ByteWriter writer) throws IOException {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final void writeTo(ByteWriter writer) throws IOException {
 
-		VarInts.writeString(writer, this.databaseName);
-		VarInts.writeString(writer, this.seriesName);
-		VarInts.writeUnsignedLong(writer, this.partition);
-		writeRecordSetTo(writer);
-	}
+        VarInts.writeString(writer, this.databaseName);
+        VarInts.writeString(writer, this.seriesName);
+        VarInts.writeUnsignedLong(writer, this.partition);
+        writeRecordSetTo(writer);
+    }
 
-	/**
-	 * Computes the serialized size of the record set.
-	 * 
-	 * @return the serialized size of the record set.
-	 */
-	protected abstract int computeRecordSetSerializedSize();
+    /**
+     * Computes the serialized size of the record set.
+     * 
+     * @return the serialized size of the record set.
+     */
+    protected abstract int computeRecordSetSerializedSize();
 
-	/**
-	 * Writes the record set to the specified writer.
-	 * 
-	 * @param writer the writer to write to
-	 * @throws IOException if an I/O problem occurs while writing the record
-	 *             set.
-	 */
-	protected abstract void writeRecordSetTo(ByteWriter writer) throws IOException;
+    /**
+     * Writes the record set to the specified writer.
+     * 
+     * @param writer the writer to write to
+     * @throws IOException if an I/O problem occurs while writing the record
+     *             set.
+     */
+    protected abstract void writeRecordSetTo(ByteWriter writer) throws IOException;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String toString() {
-		return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).append("databaseName", this.databaseName)
-		                                                                  .append("seriesName", this.seriesName)
-		                                                                  .append("partition", this.partition)
-		                                                                  .toString();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).append("databaseName", this.databaseName)
+                                                                          .append("seriesName", this.seriesName)
+                                                                          .append("partition", this.partition)
+                                                                          .toString();
+    }
 
-	protected static abstract class AbstractParser<T extends AbstractRecordBatch, S> implements Parser<T> {
+    protected static abstract class AbstractParser<T extends AbstractRecordBatch, S> implements Parser<T> {
 
-		/**
-		 * {@inheritDoc}
-		 */
-		@Override
-		public final T parseFrom(ByteReader reader) throws IOException {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public final T parseFrom(ByteReader reader) throws IOException {
 
-			String databaseName = VarInts.readString(reader);
-			String seriesName = VarInts.readString(reader);
-			long partition = VarInts.readUnsignedLong(reader);
+            String databaseName = VarInts.readString(reader);
+            String seriesName = VarInts.readString(reader);
+            long partition = VarInts.readUnsignedLong(reader);
 
-			S recordSet = parseRecordSetFrom(reader);
+            S recordSet = parseRecordSetFrom(reader);
 
-			return newRecordBatch(databaseName, seriesName, partition, recordSet);
-		}
+            return newRecordBatch(databaseName, seriesName, partition, recordSet);
+        }
 
-		/**
-		 * Parses the record set from the specified reader.
-		 * 
-		 * @param reader the reader
-		 * @return the record set.
-		 */
-		protected abstract S parseRecordSetFrom(ByteReader reader) throws IOException;
+        /**
+         * Parses the record set from the specified reader.
+         * 
+         * @param reader the reader
+         * @return the record set.
+         */
+        protected abstract S parseRecordSetFrom(ByteReader reader) throws IOException;
 
-		/**
-		 * Creates a new record batch instance.
-		 * 
-		 * @param databaseName the database name
-		 * @param seriesName the series name
-		 * @param recordSet the record set.
-		 * @param partition the partition ID
-		 * @return a new record batch instance.
-		 */
-		protected abstract T newRecordBatch(String databaseName, String seriesName, long partition, S recordSet);
+        /**
+         * Creates a new record batch instance.
+         * 
+         * @param databaseName the database name
+         * @param seriesName the series name
+         * @param recordSet the record set.
+         * @param partition the partition ID
+         * @return a new record batch instance.
+         */
+        protected abstract T newRecordBatch(String databaseName, String seriesName, long partition, S recordSet);
 
-	}
+    }
 }
